54a4ff5747f7b5d0862948e357a71c0e2480b40a
Chenxi Mao <chenxi.mao@sony.com>
2019-06-21 15:50:50 +0800

    drm: DC tuning...
    Change-Id: I6597da748e7c5b393ae8bfadcc66c08c53f79922

--- drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.h
+++ drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.h
@@ -26,7 +26,7 @@
  * @ELVSS_OFF_THRESHOLD: Minimum backlight threshold for disable smart elvss
  * @EXPOSURE_ADJUSTMENT_MIN: Minimum available PCC coefficient for OLED panel
  */
-#define ELVSS_OFF_THRESHOLD        560
+#define ELVSS_OFF_THRESHOLD        1024
 #define EXPOSURE_ADJUSTMENT_MIN    480
 
 /* PCC coefficient when exposure is 255 */
