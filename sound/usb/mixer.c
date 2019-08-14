6dbe895109dfb14a7bfe6a2ccc249b3606cfeb11
Hui Peng <benquike@gmail.com>
2019-08-13 22:34:04 -0400

    ALSA: usb-audio: Fix an OOB bug in parse_audio_mixer_unit
    commit daac07156b330b18eb5071aec4b3ddca1c377f2c upstream.
    
    The `uac_mixer_unit_descriptor` shown as below is read from the
    device side. In `parse_audio_mixer_unit`, `baSourceID` field is
    accessed from index 0 to `bNrInPins` - 1, the current implementation
    assumes that descriptor is always valid (the length  of descriptor
    is no shorter than 5 + `bNrInPins`). If a descriptor read from
    the device side is invalid, it may trigger out-of-bound memory
    access.
    
    ```
    struct uac_mixer_unit_descriptor {
    	__u8 bLength;
    	__u8 bDescriptorType;
    	__u8 bDescriptorSubtype;
    	__u8 bUnitID;
    	__u8 bNrInPins;
    	__u8 baSourceID[];
    }
    ```
    
    This patch fixes the bug by add a sanity check on the length of
    the descriptor.
    
    Reported-by: Hui Peng <benquike@gmail.com>
    Reported-by: Mathias Payer <mathias.payer@nebelwelt.net>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Hui Peng <benquike@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/usb/mixer.c
+++ sound/usb/mixer.c
@@ -1971,10 +1971,12 @@ static int parse_audio_mixer_unit(struct mixer_build *state, int unitid,
 	} else {
 		input_pins = desc->bNrInPins;
 		num_outs = uac_mixer_unit_bNrChannels(desc);
-		if (desc->bLength < 11 || !input_pins || !num_outs) {
+		if (desc->bLength < 11 || !(input_pins = desc->bNrInPins) ||
+	    	desc->bLength < sizeof(*desc) + desc->bNrInPins ||
+	    	!(num_outs = uac_mixer_unit_bNrChannels(desc))) {
 			usb_audio_err(state->chip,
-				      "invalid MIXER UNIT descriptor %d\n",
-				      unitid);
+			      "invalid MIXER UNIT descriptor %d\n",
+			      unitid);
 			return -EINVAL;
 		}
 	}
