b1e7c596c5f343f66b841c5a6785d232ec8206c8
Gah0 <woshishacha3@gmail.com>
2019-10-27 18:29:40 +0800

    sched/core: cpumask conflict
    blame on me
    commit: e0167185a9a69e8ccaafcc7fe243114290a741c6

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1198,9 +1198,6 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	if (cpumask_equal(&p->cpus_allowed, new_mask))
 		goto out;
 
-	cpumask_andnot(&allowed_mask, new_mask, cpu_isolated_mask);
-	cpumask_and(&allowed_mask, &allowed_mask, cpu_valid_mask);
-
 	dest_cpu = cpumask_any_and(cpu_valid_mask, new_mask);
 	if (dest_cpu >= nr_cpu_ids) {
 		ret = -EINVAL;
