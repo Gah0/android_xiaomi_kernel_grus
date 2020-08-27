762019b470d4bea869598e8a13eadb2f7d009950
Gah0 <woshishacha3@gmail.com>
2020-08-27 20:14:12 +0800

    msm: camera: crm: Fix use-after-free for link
    CRM gets link pointer from device private data and accesses it.
    There is a chance that pointer gets deallocated while CRM is accessing
    it. Static allocated memory will prevent use-after-free.
    
    Change-Id: Ic339f0e569d2294a5adce14c8218cd55172d1ff9
    Signed-off-by: Trishansh Bhardwaj <tbhardwa@codeaurora.org>
    Signed-off-by: Alok Chauhan <alokc@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_core.h
+++ drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_core.h
@@ -33,6 +33,8 @@
 
 #define MAX_SYNC_COUNT 65535
 
+#define MAXIMUM_LINKS_PER_SESSION  4
+
 #define SYNC_LINK_SOF_CNT_MAX_LMT 1
 
 /**
@@ -302,6 +304,10 @@ struct cam_req_mgr_connected_device {
  * @sync_link_sof_skip   : flag determines if a pkt is not available for a given
  *                         frame in a particular link skip corresponding
  *                         frame in sync link as well.
+ * @open_req_cnt         : Counter to keep track of open requests that are yet
+ *                         to be serviced in the kernel.
+ * @last_flush_id        : Last request to flush
+ * @is_used              : 1 if link is in use else 0
  *
  */
 struct cam_req_mgr_core_link {
@@ -325,6 +331,9 @@ struct cam_req_mgr_core_link {
 	int64_t                              sync_self_ref;
 	bool                                 frame_skip_flag;
 	bool                                 sync_link_sof_skip;
+	int32_t                              open_req_cnt;
+	uint32_t                             last_flush_id;
+	atomic_t                             is_used;
 	int64_t                              sync_trigger_frame_id;
 };
 
