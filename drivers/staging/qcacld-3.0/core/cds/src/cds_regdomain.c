SDM710-Development/android_kernel_xiaomi_sdm710

qcacld-3.0: Import regulatory changes from Xiaomi
Signed-off-by: Albert I <kras@raphielgang.org>

drivers/staging/qcacld-3.0/core/cds/src/cds_regdomain.c
@@ -158,7 +158,7 @@ static const struct country_code_to_reg_dmn g_all_countries[] = {
 	{CTRY_CENTRAL_AFRICA_REPUBLIC, FCC3_WORLD, "CF", "AFRICA REPUBLIC"},
 	{CTRY_CHAD, ETSI1_WORLD, "TD", "CHAD"},
 	{CTRY_CHILE, APL23_WORLD, "CL", "CHILE"},
-	{CTRY_CHINA, APL14_WORLD, "CN", "CHINA"},
+	{CTRY_CHINA, MKK5_MKKA2, "CN", "CHINA"},
 	{CTRY_CHRISTMAS_ISLAND, FCC3_WORLD, "CX", "CHRISTMAS ISLAND"},
 	{CTRY_COLOMBIA, FCC3_WORLD, "CO", "COLOMBIA"},
 	{CTRY_COSTA_RICA, FCC3_WORLD, "CR", "COSTA RICA"},
@@ -337,7 +337,7 @@ static const struct reg_dmn g_reg_dmns[] = {
 	{APL11, ETSI},
 	{APL12, ETSI},
 	{APL13, ETSI},
-	{APL14, FCC},
+	{APL14, MKK},
 	{APL15, FCC},
 	{APL16, FCC},
 	{APL17, FCC},

Gah0/android_xiaomi_kernel_grus

Signed-off-by: Gah0 <g0d3ph@gmail.com>

drivers/staging/qcacld-3.0/core/cds/src/cds_regdomain.c
@@ -158,7 +158,7 @@ static const struct country_code_to_reg_dmn g_all_countries[] = {
 	{CTRY_CENTRAL_AFRICA_REPUBLIC, FCC3_WORLD, "CF", "AFRICA REPUBLIC"},
 	{CTRY_CHAD, ETSI1_WORLD, "TD", "CHAD"},
 	{CTRY_CHILE, APL23_WORLD, "CL", "CHILE"},
-	{CTRY_CHINA, APL14_WORLD, "CN", "CHINA"},
+	{CTRY_CHINA, MKK5_MKKA2, "CN", "CHINA"},
 	{CTRY_CHRISTMAS_ISLAND, FCC3_WORLD, "CX", "CHRISTMAS ISLAND"},
 	{CTRY_COLOMBIA, FCC3_WORLD, "CO", "COLOMBIA"},
 	{CTRY_COSTA_RICA, FCC3_WORLD, "CR", "COSTA RICA"},
@@ -337,7 +337,7 @@ static const struct reg_dmn g_reg_dmns[] = {
 	{APL11, CTL_ETSI},
 	{APL12, CTL_ETSI},
 	{APL13, CTL_ETSI},
-	{APL14, CTL_FCC},
+	{APL14, CTL_MKK},
 	{APL15, CTL_FCC},
 	{APL16, CTL_FCC},
 	{APL17, CTL_FCC},