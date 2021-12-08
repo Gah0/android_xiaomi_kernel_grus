db38b1ac552b3921e0ea1bffce80dea3bd4623a5
Greg Kroah-Hartman <gregkh@google.com>
2021-12-08 09:05:25 +0100

    Merge 4.9.292 into android-4.9-q
    Changes in 4.9.292
    	staging: ion: Prevent incorrect reference counting behavour
    	USB: serial: option: add Telit LE910S1 0x9200 composition
    	USB: serial: option: add Fibocom FM101-GL variants
    	usb: hub: Fix usb enumeration issue due to address0 race
    	usb: hub: Fix locking issues with address0_mutex
    	binder: fix test regression due to sender_euid change
    	ALSA: ctxfi: Fix out-of-range access
    	staging: rtl8192e: Fix use after free in _rtl92e_pci_disconnect()
    	fuse: fix page stealing
    	xen: don't continue xenstore initialization in case of errors
    	xen: detect uninitialized xenbus in xenbus_init
    	tracing: Fix pid filtering when triggers are attached
    	ARM: dts: BCM5301X: Add interrupt properties to GPIO node
    	ASoC: topology: Add missing rwsem around snd_ctl_remove() calls
    	net: ieee802154: handle iftypes as u32
    	NFSv42: Don't fail clone() unless the OP_CLONE operation failed
    	ARM: socfpga: Fix crash with CONFIG_FORTIRY_SOURCE
    	scsi: mpt3sas: Fix kernel panic during drive powercycle test
    	drm/vc4: fix error code in vc4_create_object()
    	PM: hibernate: use correct mode for swsusp_close()
    	tcp_cubic: fix spurious Hystart ACK train detections for not-cwnd-limited flows
    	tracing: Check pid filtering when creating events
    	hugetlbfs: flush TLBs correctly after huge_pmd_unshare
    	vhost/vsock: fix incorrect used length reported to the guest
    	proc/vmcore: fix clearing user buffer by properly using clear_user()
    	NFC: add NCI_UNREG flag to eliminate the race
    	fuse: release pipe buf after last use
    	xen: sync include/xen/interface/io/ring.h with Xen's newest version
    	xen/blkfront: read response from backend only once
    	xen/blkfront: don't take local copy of a request from the ring page
    	xen/blkfront: don't trust the backend response data blindly
    	xen/netfront: read response from backend only once
    	xen/netfront: don't read data from request on the ring page
    	xen/netfront: disentangle tx_skb_freelist
    	xen/netfront: don't trust the backend response data blindly
    	tty: hvc: replace BUG_ON() with negative return value
    	shm: extend forced shm destroy to support objects from several IPC nses
    	NFSv42: Fix pagecache invalidation after COPY/CLONE
    	hugetlb: take PMD sharing into account when flushing tlb/caches
    	net: return correct error code
    	platform/x86: thinkpad_acpi: Fix WWAN device disabled issue after S3 deep
    	s390/setup: avoid using memblock_enforce_memory_limit
    	thermal: core: Reset previous low and high trip during thermal zone init
    	scsi: iscsi: Unblock session then wake up error handler
    	ethernet: hisilicon: hns: hns_dsaf_misc: fix a possible array overflow in hns_dsaf_ge_srst_by_port()
    	net: tulip: de4x5: fix the problem that the array 'lp->phy[8]' may be out of bound
    	net: ethernet: dec: tulip: de4x5: fix possible array overflows in type3_infoblock()
    	vrf: Reset IPCB/IP6CB when processing outbound pkts in vrf dev xmit
    	kprobes: Limit max data_size of the kretprobe instances
    	sata_fsl: fix UAF in sata_fsl_port_stop when rmmod sata_fsl
    	sata_fsl: fix warning in remove_proc_entry when rmmod sata_fsl
    	fs: add fget_many() and fput_many()
    	fget: check that the fd still exists after getting a ref to it
    	natsemi: xtensa: fix section mismatch warnings
    	net: qlogic: qlcnic: Fix a NULL pointer dereference in qlcnic_83xx_add_rings()
    	siphash: use _unaligned version by default
    	net/rds: correct socket tunable error in rds_tcp_tune()
    	parisc: Fix "make install" on newer debian releases
    	vgacon: Propagate console boot parameters before calling `vc_resize'
    	tty: serial: msm_serial: Deactivate RX DMA for polling support
    	serial: pl011: Add ACPI SBSA UART match id
    	serial: core: fix transmit-buffer reset and memleak
    	Linux 4.9.292
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I98a677406cfca6a63fffa4a94e45edbd45fd671a

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4495,8 +4495,6 @@ hub_port_init(struct usb_hub *hub, struct usb_device *udev, int port1,
 	if (oldspeed == USB_SPEED_LOW)
 		delay = HUB_LONG_RESET_TIME;
 
-	mutex_lock(hcd->address0_mutex);
-
 	/* Reset the device; full speed may morph to high speed */
 	/* FIXME a USB 2.0 device may morph into SuperSpeed on reset. */
 	retval = hub_port_reset(hub, port1, udev, delay, false);
@@ -4784,7 +4782,6 @@ fail:
 		hub_port_disable(hub, port1, 0);
 		update_devnum(udev, devnum);	/* for disconnect processing */
 	}
-	mutex_unlock(hcd->address0_mutex);
 	return retval;
 }
 
@@ -4875,6 +4872,7 @@ static void hub_port_connect(struct usb_hub *hub, int port1, u16 portstatus,
 	struct usb_device *udev = port_dev->child;
 	static int unreliable_port = -1;
 	enum usb_device_speed dev_speed = USB_SPEED_UNKNOWN;
+	bool retry_locked;
 
 	/* Disconnect any existing devices under this port */
 	if (udev) {
@@ -4937,7 +4935,11 @@ static void hub_port_connect(struct usb_hub *hub, int port1, u16 portstatus,
 
 retry_enum:
 	status = 0;
+
 	for (i = 0; i < SET_CONFIG_TRIES; i++) {
+		usb_lock_port(port_dev);
+		mutex_lock(hcd->address0_mutex);
+		retry_locked = true;
 
 		/* reallocate for each attempt, since references
 		 * to the previous one can escape in various ways
@@ -4946,6 +4948,8 @@ retry_enum:
 		if (!udev) {
 			dev_err(&port_dev->dev,
 					"couldn't allocate usb_device\n");
+			mutex_unlock(hcd->address0_mutex);
+			usb_unlock_port(port_dev);
 			goto done;
 		}
 
@@ -4967,11 +4971,11 @@ retry_enum:
 		}
 
 		/* reset (non-USB 3.0 devices) and get descriptor */
-		usb_lock_port(port_dev);
 		status = hub_port_init(hub, udev, port1, i);
-		usb_unlock_port(port_dev);
 		if (status < 0)
 			goto loop;
+
+		mutex_unlock(hcd->address0_mutex);
 
 		dev_speed = udev->speed;
 		if (udev->speed > USB_SPEED_UNKNOWN &&
@@ -4979,6 +4983,9 @@ retry_enum:
 				&& hcd->usb_phy->disable_chirp)
 			hcd->usb_phy->disable_chirp(hcd->usb_phy,
 					false);
+
+		usb_unlock_port(port_dev);
+		retry_locked = false;
 
 		if (udev->quirks & USB_QUIRK_DELAY_INIT)
 			msleep(2000);
@@ -5072,6 +5079,10 @@ loop:
 		usb_ep0_reinit(udev);
 		release_devnum(udev);
 		hub_free_dev(udev);
+		if (retry_locked) {
+			mutex_unlock(hcd->address0_mutex);
+			usb_unlock_port(port_dev);
+		}
 		usb_put_dev(udev);
 		if ((status == -ENOTCONN) || (status == -ENOTSUPP))
 			break;
@@ -5636,6 +5647,8 @@ static int usb_reset_and_verify_device(struct usb_device *udev)
 	bos = udev->bos;
 	udev->bos = NULL;
 
+	mutex_lock(hcd->address0_mutex);
+
 	for (i = 0; i < SET_CONFIG_TRIES; ++i) {
 
 		/* ep0 maxpacket size may change; let the HCD know about it.
@@ -5645,6 +5658,7 @@ static int usb_reset_and_verify_device(struct usb_device *udev)
 		if (ret >= 0 || ret == -ENOTCONN || ret == -ENODEV)
 			break;
 	}
+	mutex_unlock(hcd->address0_mutex);
 
 	if (ret < 0)
 		goto re_enumerate;
